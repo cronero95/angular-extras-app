5d24229f1067ef4085e3a31914060fc641923d91
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-04-09 13:45:37 -0500

    Prepare project structure

---
+++ src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-heavy-loader-fast',
+  standalone: true,
+  imports: [],
+  template: `
+  <h1>Heavy loader fast works!!</h1>
+  `,
+  styles: ``
+})
+export class HeavyLoaderFastComponent {
+
+}
