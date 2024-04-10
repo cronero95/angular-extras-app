fdebbe993d0f8232a1abbafb951e9367e7222e1b
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-04-10 16:51:38 -0500

    Change detection behavior

--- src/app/dashboard/pages/change-detection-page/change-detection-page.component.ts
+++ src/app/dashboard/pages/change-detection-page/change-detection-page.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    TitleComponent,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './change-detection-page.component.html',
   styles: ``
 })
 export default class ChangeDetectionPageComponent {
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  public currentFramework = computed(() => `Change Detection - ${this.frameworkAsSignal().name}`);
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+
+      this.frameworkAsSignal.update(value => {
+        value.name = 'React';
+        return {...value};
+      })
+
+      console.log('done');
+    }, 3000);
+  }
 
 }
