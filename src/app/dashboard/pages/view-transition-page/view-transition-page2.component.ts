5937ef95f66f5dcff1088afb7a3f7b7b47fc9345
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-04-10 22:48:36 -0500

    Add animation with heroAnimation

---
+++ src/app/dashboard/pages/view-transition-page/view-transition-page2.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
+
+@Component({
+  standalone: true,
+  imports: [
+    TitleComponent,
+  ],
+  template: `
+  <shared-title title="View Transition 2"/>
+  <section class="flex justify-end">
+
+    <img
+      srcset="https://picsum.photos/id/237/200/300"
+      alt="A black dog"
+      width="200"
+      height="300"
+      style="view-transition-name: hero-1"
+    >
+
+    <div
+      class="bg-blue-500 w-56 h-56"
+      style="view-transition-name: hero-2"
+    ></div>
+
+  </section>
+  `,
+  styles: ``
+})
+export default class ViewTransitionPageComponent {
+
+}
